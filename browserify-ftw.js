e5803d4695128a63badd021960c381cd9e1ca75a
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-15 16:10:15 -0500

    separating into modules

--- browserify-ftw.js
+++ browserify-ftw.js
@@ -1,15 +1,9 @@
 'use strict';
 
-var esprima = require('esprima')
-  , assert = require('assert')
+var getWrapper = require('./lib/get-wrapper')
+  , requirefy = require('./lib/requirefy')
+  , exportify = require('./lib/exportify')
   , util = require('util')
-  , syntax = esprima.Syntax
-  , parse  = esprima.parse
-  , format = util.format
-  , esprimaOpts = {
-        range: true
-      , raw: true
-    }
   , options = {
         quote: '\''
       , style: 'var'
@@ -21,125 +15,23 @@ function inspect(obj, depth) {
   console.log(util.inspect(obj, false, depth || 5, true));
 }
 
-function getWrapper(ast) {
-  var wrappers = ast.body
-    .map(function (node) {
-      if (node.type !== syntax.ExpressionStatement) return null;
-
-      var expression = node.expression;
-      if (!expression) return null;
-      if (expression.type !== syntax.CallExpression) return null;
-
-      var name = expression.callee.name;
-      if (name !== 'define' && name !== 'require') return null;
-
-      var args = expression.arguments;
-      if (!args.length) return null;
-
-      // we found a requirejs wrapper
-      var paths, fn;
-      if (args[0].type === syntax.FunctionExpression) {
-        // define(callback)
-        paths = [];
-        fn = args[0];
-      } else {
-        // define([..], callback)
-        paths = args[0].elements.map(function (x) { return x.value; });
-        fn = args[1];
-      }
-
-      // Examine callback function
-      var params = fn.params.map(function (x) { return x.name; });
-
-      return { 
-          name: name
-        , paths: paths
-        , params: params
-        , body: fn.body.body
-        , head: { start: expression.range[0],   end: fn.body.range[0] + 1 }
-        , tail: { start: fn.body.range[1] - 1,  end: expression.range[1] + 1 }
-      };
-    })
-    .filter(function (x) { return x; });
-  return wrappers.length ? wrappers[0] : null;
-}
-
-function getReturnStatement(body) {
-  // only finds top level return statements (i.e. only one)
-  // no conditional returns are (i.e. inside if/else block) are considered
-  var returns = body
-    .map(function (node) {
-      if (node.type !== syntax.ReturnStatement) return null;
-      
-      var arg = node.argument;
-      if (!arg) return null;
-
-      return {
-          argument: { type: arg.type, start: arg.range[0], end: arg.range[1] }
-        , start: node.range[0]
-        , end: node.range[1]
-      };
-    })
-    .filter(function (x) { return x; });
-
-  return returns.length ? returns[0] : null;
-}
-
-function generateHead(wrapper) {
-  var requires = [];
-
-  assert(wrapper.paths.length === wrapper.params.length, 'number of paths should match number of parameters to callback');
-
-  for (var i = 0; i < wrapper.paths.length; i++) 
-    requires.push(format('%s = require(%s%s%s)', wrapper.params[i], options.quote, wrapper.paths[i], options.quote));
-
-  var head;
-  switch (options.style) {
-    case 'var': head = requires.map(function (r) {
-      return 'var ' + r + ';';  
-    }).join('\n');
-  }
-  return head;
-}
-
-function removeRequirejs(code, head, tail) {
-  var top = reqjs.substring(0, head.start);
-  var mid = reqjs.substring(head.end, tail.start);
-  var bot = reqjs.substring(tail.end);
-
-  return top + mid + bot;
-}
-
-function indentRegexp(indent) {
-  for (var i = 1, spaces = ' '; i < indent; i++) spaces += ' ';
-  return new RegExp('^' + spaces);
-}
-
-function fixIndent(line, regex) {
-  return line.replace(regex, '');
-}
-
 var fs = require('fs');
 
-var reqjs = fs.readFileSync('./test/fixtures/define-multiline.js', 'utf-8');
+var code = fs.readFileSync('./test/fixtures/define-multiline.js', 'utf-8');
 
-//"define([ 'director' ], function (director) {  return 1; })";
+/*
+ * Need to parse AST twice, since ranges go out of sync everytime we modify the code
+ * 1. Parse code, find return statement inside requirejs wrapper and replace with assignment to module.exports
+ *    This needs to happen before removing the wrapper since requirejs considers returns on script level to be illegal.
+ * 2. Parse resulting code, find and replace requirejs wrapper with appropriate requirejs statements
+ */
 
-var ast = parse(reqjs, esprimaOpts);
+var wrapper;
 
-var wrapper = getWrapper(ast);
-var head = generateHead(wrapper);
-var bare = removeRequirejs(reqjs, wrapper.head, wrapper.tail);
-var regex = indentRegexp(options.indent);
+wrapper = getWrapper(code, true);
+var exportified = exportify(code, wrapper.return);
 
-var properlyIndented = bare
-  .split('\n')
-  .map(function (x) { return fixIndent(x, regex); } )
-  .join('\n');
+wrapper = getWrapper(exportified, false);
+var requirefied = requirefy(exportified, options);
 
-var requirefied = head + '\n' + properlyIndented;
-
-
-
-var returnStmt = getReturnStatement(wrapper.body);
-inspect(returnStmt);
+console.log(requirefied);
