f0c180e0b994378c1d7120a7786b37ccbe5791ae
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-16 19:05:26 -0500

    cli working by passing in requirejs config and refactor config paths

--- bin/browserify-ftw.js
+++ bin/browserify-ftw.js
@@ -2,26 +2,55 @@
 'use strict';
 
 var util    =  require('util')
+  , fs = require('fs')
   , path    =  require('path')
   , log     =  require('npmlog')
   , bftw    =  require('../browserify-ftw')
-  , options =  {
-        quote           :  '\''
-      , style           :  'var'
-      , indent          :  2
-      , directoryFilter :  null
-      , fileFilter      :  '.js'
-      , dryrun          :  true
-    }
-  ;
+  , options;
 
 function inspect(obj, depth) {
   console.log(util.inspect(obj, false, depth || 5, true));
 }
 
-var fullPathToRequireJsConfig = path.join(__dirname, '../test/fixtures/requirejs-config.js');
+function printUsage() {
+  log.info('browserify-ftw', 'Usage:\n\t browserify-ftw /path/to/reqirejs-config.js [/path/to/refactor-config.js]');
+}
+
+var args = process.argv.slice(2);
+if (!args.length) { 
+  log.error('browserify-ftw', 'Missing path to requirejs config file');
+  printUsage();
+}
+
+var requirejsConfigPath = args[0]
+  , refactorConfigPath = args[1] || path.join(__dirname, '../lib/refactor-config.js')
+  , fullPathToRequireJsConfig
+  , fullPathToRefactorConfig
+  ;
+
+try {
+  fullPathToRequireJsConfig = fs.realpathSync(requirejsConfigPath);
+  fullPathToRefactorConfig = fs.realpathSync(refactorConfigPath);
+} catch (e) {
+  log.error('browserify-ftw', 'While resolving given path(s):\n\t', e.message);
+  process.exit(1);
+}
+
+try {
+  options = require(fullPathToRefactorConfig);
+  // Poor man's validation
+  if (!options.quote || !options.style) throw new Error(fullPathToRefactorConfig + 'is not a valid refactor config');
+} catch (e) {
+  log.error('browserify-ftw', 'While reading refactor-config:\n\t', e.message);
+  process.exit(1);
+}
 
 bftw(fullPathToRequireJsConfig, options, function (err) {
-  if (err) return log.error('browserify-ftw', err);
+  if (err) {
+    log.error('browserify-ftw', err);
+    return process.exit(1);
+  }
+
   log.info('browserify-ftw', 'Successfully upgraded your project. Please run valiquire "npm install -g valiquire" to validate all require paths.');
+  process.exit(0);
 });
