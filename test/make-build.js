344340e84f58257bd981ad648c7e7b84bc290e80
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-23 16:20:17 -0500

    adding test that shows that indent and quotes are respected when build script is generated

--- test/make-build.js
+++ test/make-build.js
@@ -12,33 +12,54 @@ function inspect(obj, depth) {
   return require('util').inspect(obj, false, depth || 5, true);
 }
 
-var config = {
-    quote: '\''
-  , indent: 2
-  , entryPath: './js/entry.js'
-};
-var expected = fs.readFileSync(path.join(generated, 'without-shims.js'), 'utf-8')
+~function () {
+  var config = {
+      quote: '\''
+    , indent: 2
+    , entryPath: './js/entry.js'
+  };
+  var expected = fs.readFileSync(path.join(generated, 'without-shims.js'), 'utf-8')
 
-test('\nwhen generating browserify build when no modules need to be shimmed:\n' + inspect(config) + '\nGenerates:\n' + cardinal.highlight(expected),  function (t) {
-  t.equals(makeBuild(config, './build/bundle.js'), expected)
-  t.end();
-});
+  test('\nwhen generating browserify build when no modules need to be shimmed:\n' + inspect(config) + '\nGenerates:\n' + cardinal.highlight(expected),  function (t) {
+    t.equals(makeBuild(config, './build/bundle.js'), expected)
+    t.end();
+  });
+}();
 
-var shims = [ 
-    { alias: 'jquery', path: './js/vendor/jquery.js', export: '$' }
-  , { alias: 'underscore', path: './js/vendor/underscore.js', export: null }
-];
+~function () {
+  var config = {
+      quote: '"'
+    , indent: 4
+    , entryPath: './js/entry.js'
+  };
 
-config = {
-    quote: '\''
-  , indent: 2
-  , entryPath: './js/entry.js'
-  , shims: shims
-};
+  var expected = fs.readFileSync(path.join(generated, 'without-shims-tabsize-4-and-doublequotes.js'), 'utf-8')
 
-expected = fs.readFileSync(path.join(generated, 'with-two-shims.js'), 'utf-8')
+  test('\nwhen generating browserify build when no modules need to be shimmed::\n' + inspect(config) + '\nGenerates:\n' + cardinal.highlight(expected),  function (t) {
+  console.log(makeBuild(config, './build/bundle.js'))
+  console.log('expected: ', expected);
+    t.equals(makeBuild(config, './build/bundle.js'), expected)
+    t.end();
+  });
+}();
 
-test('\nwhen generating browserify build when two modules need to be shimmed:\n' + inspect(config) + '\nGenerates:\n' + cardinal.highlight(expected),  function (t) {
-  t.equals(makeBuild(config, './build/bundle.js'), expected)
-  t.end();
-});
+~function () {
+  var shims = [ 
+      { alias: 'jquery', path: './js/vendor/jquery.js', export: '$' }
+    , { alias: 'underscore', path: './js/vendor/underscore.js', export: null }
+  ];
+
+  var config = {
+      quote: '\''
+    , indent: 2
+    , entryPath: './js/entry.js'
+    , shims: shims
+  };
+
+  var expected = fs.readFileSync(path.join(generated, 'with-two-shims.js'), 'utf-8')
+
+  test('\nwhen generating browserify build when two modules need to be shimmed:\n' + inspect(config) + '\nGenerates:\n' + cardinal.highlight(expected),  function (t) {
+    t.equals(makeBuild(config, './build/bundle.js'), expected)
+    t.end();
+  });
+}()
