3212ee9f9d5d3df2f63d0a0fa865fb52b5ac824b
Thorsten Lorenz <thlorenz@gmx.de>
2012-12-27 21:39:09 -0500

    resolving correct shim paths when build.js is in different folder than requirejs config'

--- test/prepare-shims.js
+++ test/prepare-shims.js
@@ -26,7 +26,12 @@ var paths = {
       }
     }
 
-  , expected = { 
+function inspect(obj, depth) {
+ return require('util').inspect(obj, false, depth || 5, true);
+}
+
+~function () {
+  var expected = { 
     shims: [ { exports: '$',
         alias: 'jquery',
         path: 'vendor/jquery-1.8.2' },
@@ -44,11 +49,33 @@ var paths = {
       } ],
     missingPaths: [ 'handlebars' ] }
   
-function inspect(obj, depth) {
- return require('util').inspect(obj, false, depth || 5, true);
-}
+  test('requireJS dir and bundleJS dir is the same and given paths:\n' + inspect(paths) + '\nand shims:\n' + inspect(shims) + '\nit returns:\n' + inspect(expected), function (t) {
+    t.deepEquals(prepareShims(shims, paths, '.', '.'), expected)
+    t.end()
+  })
+}();
 
-test('given paths:\n' + inspect(paths) + '\nand shims:\n' + inspect(shims) + '\nit returns:\n' + inspect(expected), function (t) {
-  t.deepEquals(prepareShims(shims, paths), expected)
-  t.end()
-})
+~function () {
+  var expected = { 
+    shims: [ { exports: '$',
+        alias: 'jquery',
+        path: '../vendor/jquery-1.8.2' },
+      { deps: [ 'underscore', 'jquery' ],
+        exports: 'Backbone',
+        alias: 'backbone',
+        path: '../vendor/backbone' },
+      { deps: [ 'backbone' ],
+        alias: 'backbone.stickit',
+        path: '../vendor/backbone.stickit-mod' },
+      { exports: 'Handlebars',
+        alias: 'handlebars', 
+        pathMissing: true,
+        path: prepareShims.pathMissingFor('handlebars') 
+      } ],
+    missingPaths: [ 'handlebars' ] }
+  
+  test('requireJS is "." and buildJS dir is "./build" the same and given paths:\n' + inspect(paths) + '\nand shims:\n' + inspect(shims) + '\nit returns:\n' + inspect(expected), function (t) {
+    t.deepEquals(prepareShims(shims, paths, '.', './build'), expected)
+    t.end()
+  })
+}();
