caadcd72cec30051199f58d95f41c92ad9164942
Thorsten Lorenz <thlorenz@gmx.de>
2013-01-18 07:38:32 -0500

    removing bottom .shim() in generated build, since that is no longer needed with the current browerify-shim version

--- test/fixtures/generated/with-two-shims.js
+++ test/fixtures/generated/with-two-shims.js
@@ -7,7 +7,6 @@ var bundled = browserify({ debug: true })
   .use(shim({ alias: 'jquery', path: './js/vendor/jquery.js', exports: '$' }))
   .use(shim({ alias: 'underscore', path: './js/vendor/underscore.js', exports: null }))
   .addEntry(path.join(__dirname, './js/entry.js'))
-  .bundle()
-  .shim();
+  .bundle();
 
 fs.writeFileSync(path.join(__dirname, './build/bundle.js'), bundled, 'utf-8');
