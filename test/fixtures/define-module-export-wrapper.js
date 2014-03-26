5d3db47a8f95be4b5c55b5284e0e7526c5ef8535
Andrew de Andrade <andrew@deandrade.com.br>
2014-03-25 23:35:13 -0700

    Preserve header with define commonjs module exports wrapper

---
+++ test/fixtures/define-module-export-wrapper.js
@@ -0,0 +1,25 @@
+/* This comment block here is a placeholder for what would normally be a license 
+* block such as the MPL-2.0. After browserify-ftw rewrites this file, it should 
+* still be at the top of the file before the variable assignments.
+*/
+
+define(function (require, exports, module) { 
+  var director = require('director');
+  var github = require('github-index');
+  var blog = require('blog-index');
+  var about = require('about-index');
+  var githubContent = require('github-content');
+  var blogContent = require('blog-content');
+  var aboutContent = require('about-content');
+  var foo;
+
+  var currentNav = null
+    , navs = {
+          github        :  github.init
+        , blog          :  blog.init
+        , about         :  about.init
+      }
+    ;
+
+  module.exports = navs;
+});
---
+++ test/fixtures/define-module-export-wrapper.upgraded.js
@@ -0,0 +1,23 @@
+/* This comment block here is a placeholder for what would normally be a license 
+* block such as the MPL-2.0. After browserify-ftw rewrites this file, it should 
+* still be at the top of the file before the variable assignments.
+*/
+
+var director = require('./resolved/director');
+var github = require('./resolved/github-index');
+var blog = require('./resolved/blog-index');
+var about = require('./resolved/about-index');
+var githubContent = require('./resolved/github-content');
+var blogContent = require('./resolved/blog-content');
+var aboutContent = require('./resolved/about-content');
+var foo;
+
+var currentNav = null
+  , navs = {
+        github        :  github.init
+      , blog          :  blog.init
+      , about         :  about.init
+    }
+  ;
+
+module.exports = navs;
