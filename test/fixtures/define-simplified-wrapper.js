488c9371d3da507a819f2e22081a84d78ce3b1af
Andrew de Andrade <andrew@deandrade.com.br>
2014-03-28 15:11:29 -0700

    Preserve comments in define/require but before require var decls

--- test/fixtures/define-simplified-wrapper.js
+++ test/fixtures/define-simplified-wrapper.js
@@ -1,9 +1,12 @@
 /* This comment block here is a placeholder for what would normally be a license 
-* block such as the MPL-2.0. After browserify-ftw rewrites this file, it should 
-* still be at the top of the file before the variable assignments.
-*/
+ * block such as the MPL-2.0. After browserify-ftw rewrites this file, it should 
+ * still be at the top of the file before the variable assignments.
+ */
 
-define(function (require) { 
+define(function (require) {
+  /* This is a comment block within the RequireJS wrapper. It should be 
+   * preserved as well after conversion. 
+   */
   var director = require('director');
   var github = require('github-index');
   var blog = require('blog-index');
--- test/fixtures/define-simplified-wrapper.upgraded.js
+++ test/fixtures/define-simplified-wrapper.upgraded.js
@@ -1,8 +1,11 @@
 /* This comment block here is a placeholder for what would normally be a license 
-* block such as the MPL-2.0. After browserify-ftw rewrites this file, it should 
-* still be at the top of the file before the variable assignments.
-*/
+ * block such as the MPL-2.0. After browserify-ftw rewrites this file, it should 
+ * still be at the top of the file before the variable assignments.
+ */
 
+/* This is a comment block within the RequireJS wrapper. It should be 
+ * preserved as well after conversion. 
+ */
 var director = require('./resolved/director');
 var github = require('./resolved/github-index');
 var blog = require('./resolved/blog-index');
