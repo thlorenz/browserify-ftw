a75369af25bbd16fc9a882fd4133232c33432bfc
Simen Bekkhus <sbekkhus91@gmail.com>
2015-04-23 20:54:56 +0200

    Add option to move 'use strict'; statement to top of file
    Fixes #14

---
+++ test/fixtures/use-strict-nested-not-moved.js
@@ -0,0 +1,15 @@
+define([ 'director' ], function (director) {
+  var currentNav = null;
+
+  var nested = function() {
+    'use strict';
+
+    // A comment
+  };
+
+  return {
+      github        :  github.init
+    , blog          :  blog.init
+    , about         :  about.init
+  };
+});
